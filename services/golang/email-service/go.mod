ae35e7daf3c976058a088875897a1fe645ee576f
NGOVANMANH <vanmanh0888@gmail.com>
2025-06-11 18:32:45 +0700

    update: email servce

--- services/golang/email-service/go.mod
+++ services/golang/email-service/go.mod
@@ -3,3 +3,9 @@ module email-service
 go 1.24.0
 
 require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/segmentio/kafka-go v0.4.48 // indirect
+)
--- services/golang/email-service/internal/kafka/consumer.go
+++ services/golang/email-service/internal/kafka/consumer.go
@@ -1,4 +1,29 @@
 package kafka
 
-func StartComsumer() {
+import (
+	"context"
+	"log"
+
+	kafka_lib "github.com/segmentio/kafka-go"
+)
+
+func ConsumeUserRegisterEvents(brokers []string, topic string, groupID string, handleFunc func(message kafka_lib.Message)) {
+	r := kafka_lib.NewReader(kafka_lib.ReaderConfig{
+		Brokers:  brokers,
+		GroupID:  groupID,
+		Topic:    topic,
+		MinBytes: 10e3, 
+		MaxBytes: 10e6, 
+	})
+	defer r.Close()
+
+	ctx := context.Background()
+	for {
+		m, err := r.ReadMessage(ctx)
+		if err != nil {
+			log.Printf("error while reading message: %v", err)
+			continue
+		}
+		handleFunc(m)
+	}
 }
